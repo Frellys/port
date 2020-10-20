8301a41dca94e3c7bf96c6f87a98f119b2080693
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-20 13:28:59 +0000

    Add files via upload

--- leetcode/medium/Integer_to_Roman.js
+++ leetcode/medium/Integer_to_Roman.js
@@ -8,32 +8,53 @@
 var intToRoman = function (num) {
     let res = '';
     num.toString().split('').forEach(function (el, idx, arr) {
-        let base;
-        let diff;
+        let base = '';
+        let diff = '';
+        let cur = '';
         switch (arr.length - idx) {
             case 0: {
                 base = (el > 5) ? 'X' : 'V';
                 diff = 'I';
+                base = (el > 5) ? 'M' : 'D';
+                diff = 'C';
+                for (let i = 0; i < Math.abs(5 - el); i++) {
+                    cur += diff;
+                }
+                cur = (el > 5) ? base + cur : base - cur;
                 break;
             }
             case 1: {
                 base = (el > 5) ? 'C' : 'L';
                 diff = 'X';
+                base = (el > 5) ? 'M' : 'D';
+                diff = 'C';
+                for (let i = 0; i < Math.abs(5 - el); i++) {
+                    cur += diff;
+                }
+                cur = (el > 5) ? base + cur : base - cur;
                 break;
             }
             case 2: {
                 base = (el > 5) ? 'M' : 'D';
                 diff = 'C';
+                for (let i = 0; i < Math.abs(5 - el); i++) {
+                    cur += diff;
+                }
+                cur = (el > 5) ? base + cur : base - cur;
                 break;
             }
-            //case 3: {
-            //    base = (el > 5) ? 'M' : 'D';
-            //    diff = 'C';
-            //    break;
-            //}
+            case 3: {
+                base = 'M';
+                for (let i = 0; i < el; i++) {
+                    cur += base;
+                }
+                break;
+            }
             default: {
                 break;
             }
         }
+        res += cur;
     });
+    return res;
 };
