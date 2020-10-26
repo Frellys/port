1b465501eb79d3dc8a803142d2387af734676ff5
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-26 10:17:58 +0000

    Add files via upload

--- leetcode/easy/Missing_Number.js
+++ leetcode/easy/Missing_Number.js
@@ -6,5 +6,9 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    for (let i = 0; i < nums.length; i++) { }
+    for (let i = 0; i < nums.length + 1; i++) {
+        if (!nums.includes(i)) {
+            return i;
+        }
+    }
 };
