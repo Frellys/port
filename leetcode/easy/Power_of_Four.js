0f3ac4c24c52b02bd209355dff53d2d7fc5b689a
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-28 08:53:17 +0000

    Add files via upload

---
+++ leetcode/easy/Power_of_Four.js
@@ -0,0 +1,9 @@
+// Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function (num) {
+
+};
