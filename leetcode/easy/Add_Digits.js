65fe934b4a9557cabee5667010fd26cd906a3c7a
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-28 06:45:44 +0000

    Add files via upload

---
+++ leetcode/easy/Add_Digits.js
@@ -0,0 +1,9 @@
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+
+};
