2ed9a0792c545a3f350b74e50f15004368165a5a
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-28 08:47:09 +0000

    Add files via upload

--- leetcode/easy/Word_Pattern.js
+++ leetcode/easy/Word_Pattern.js
@@ -7,5 +7,21 @@
  * @return {boolean}
  */
 var wordPattern = function (pattern, s) {
-
+    let words = s.split(' ');
+    if (pattern.length != words.length) {
+        return false;
+    }
+    for (let i = 0; i < pattern.length; i++) {
+        let char = pattern[i];
+        let word = words[i];
+        for (let j = 0; j < pattern.length; j++) {
+            if (char == pattern[j]) {
+                if (word != words[j]) return false;
+            }
+            if (word == words[j]) {
+                if (char != pattern[j]) return false;
+            }
+        }
+    }
+    return true;
 };
