6a537662b9363abe6010db06b2424d9aa78b0f6f
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-28 12:36:33 +0500

    Delete Add_Digits.js

--- leetcode/easy/Add_Digits.js
+++
@@ -1,9 +0,0 @@
-// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
-
-/**
- * @param {number} num
- * @return {number}
- */
-var addDigits = function (num) {
-
-};
