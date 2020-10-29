e33d5fc22777d2f5fe91a76ac7d1d8aa3b3fccba
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-29 15:23:18 +0000

    Add files via upload

--- leetcode/easy/Keyboard_Row.js
+++ leetcode/easy/Keyboard_Row.js
@@ -7,5 +7,30 @@
  * @return {string[]}
  */
 var findWords = function (words) {
-
+    let kbd = [
+        { cur: false, charset: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'] },
+        { cur: false, charset: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'] },
+        { cur: false, charset: ['z', 'x', 'c', 'v', 'b', 'n', 'm'] }
+    ];
+    let ret = [];
+    words.forEach(function (word) {
+        word.split('').forEach(function (char, idx) {
+            let ch = char.toLowerCase();
+            if (idx == 0) {
+                kbd.forEach(function (row) {
+                    if (row.charset.includes(ch)) row.cur = true;
+                });
+            }
+            else {
+                kbd.forEach(function (row) {
+                    if (row.cur && !row.charset.includes(ch)) { row.cur = false; }
+                });
+            }
+        });
+        kbd.forEach(function (row) {
+            if (row.cur) ret.push(word);
+            row.cur = false;
+        });
+    });
+    return ret;
 };
