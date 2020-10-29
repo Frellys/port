51776276a5640b648f29d67ebdfbef288d43ff81
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-29 16:55:52 +0000

    Add files via upload

---
+++ leetcode/easy/Array_Partition_I.js
@@ -0,0 +1,17 @@
+// Given an integer array nums of 2n integers, group these integers into n pairs(a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized.
+// Return the maximized sum.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function (nums) {
+    let sum = 0;
+    nums.sort(function (a, b) {
+        return a - b;
+    });
+    for (let i = 0; i < nums.length; i += 2) {
+        sum += Math.min(nums[i], nums[i + 1]);
+    }
+    return sum;
+};
