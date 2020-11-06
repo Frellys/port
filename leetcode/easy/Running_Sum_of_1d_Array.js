e4d2aa286e42ffab60be1b4ecdddac1109d28239
Frellys <50921387+Frellys@users.noreply.github.com>
2020-11-06 06:44:45 +0000

    Add files via upload

---
+++ leetcode/easy/Running_Sum_of_1d_Array.js
@@ -0,0 +1,18 @@
+// Given an array nums.We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+    let ret = [];
+    for (let i = 0; i < nums.length; i++) {
+        let n = 0;
+        for (let j = 0; j <= i; j++) {
+            n += nums[j];
+        }
+        ret.push(n);
+    }
+    return ret;
+};
