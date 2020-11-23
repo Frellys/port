f91cffbd586e5d81827f088c39bf3db08ac7cae2
Frellys <50921387+Frellys@users.noreply.github.com>
2020-11-23 10:33:52 +0500

    Add files via upload

---
+++ leetcode/easy/Base 7.js
@@ -0,0 +1,9 @@
+// Given an integer, return its base 7 string representation.
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+    return num.toString(7);
+};
