1f80672b40a96482e67cbb5c9c06c6420ef03721
Frellys <50921387+Frellys@users.noreply.github.com>
2020-11-27 14:10:19 +0500

    Delete Delete Duplicate Emails.sql

--- leetcode/easy/Delete Duplicate Emails.sql
+++
@@ -1,3 +0,0 @@
--- Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
-DELETE t1 FROM Person t1, Person t2
-WHERE (t1.Id > t2.Id) AND (t1.Email = t2.Email);
