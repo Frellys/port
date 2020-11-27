39c6f87e165c0010d3ca1f163b2d96cc72abd408
Frellys <50921387+Frellys@users.noreply.github.com>
2020-11-27 14:10:29 +0500

    Delete Second Highest Salary.sql

--- leetcode/easy/Second Highest Salary.sql
+++
@@ -1,4 +0,0 @@
---Write a SQL query to get the second highest salary from the Employee table.
-SELECT MAX(Salary) AS SecondHighestSalary
-FROM Employee
-WHERE Salary < (SELECT MAX(Salary) FROM Employee);
