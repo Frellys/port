d132009d45ab6600a7f6bd728b06ddcfe9a0f8a9
Frellys <50921387+Frellys@users.noreply.github.com>
2021-01-22 13:38:20 +0500

    Add files via upload

---
+++ leetcode/easy/Toeplitz Matrix.js
@@ -0,0 +1,22 @@
+// Given an m x n matrix, return true if the matrix is Toeplitz.Otherwise, return false.
+// A matrix is Toeplitz if every diagonal from top - left to bottom - right has the same elements.
+
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function (matrix) {
+    let flag = true;
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    matrix.forEach(function (row, rdx) {
+        if (flag && rdx > 0) {
+            row.forEach(function (col, cdx) {
+                if (cdx > 0) {
+                    if (matrix[rdx - 1][cdx - 1] != col) flag = false;
+                }
+            });
+        }
+    });
+    return flag;
+};
