d7e4eaad540f2452790e34246de561606996ac62
Frellys <50921387+Frellys@users.noreply.github.com>
2021-01-22 13:39:03 +0500

    Add files via upload

--- leetcode/easy/Toeplitz Matrix.js
+++ leetcode/easy/Toeplitz Matrix.js
@@ -7,8 +7,6 @@
  */
 var isToeplitzMatrix = function (matrix) {
     let flag = true;
-    let rows = matrix.length;
-    let cols = matrix[0].length;
     matrix.forEach(function (row, rdx) {
         if (flag && rdx > 0) {
             row.forEach(function (col, cdx) {
