122fb67ee534f5f3050b92184dcd4b696f7153d4
Frellys <50921387+Frellys@users.noreply.github.com>
2021-04-02 11:39:40 +0500

    Add files via upload

---
+++ leetcode/easy/Number of Different Integers in a String.js
@@ -0,0 +1,27 @@
+// You are given a string word that consists of digits and lowercase English letters.
+// You will replace every non - digit character with a space.For example, "a123bc34d8ef34" will become " 123  34 8  34".
+// Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+// Return the number of different integers after performing the replacement operations on word.
+// Two integers are considered different if their decimal representations without any leading zeros are different.
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numDifferentIntegers = function (word) {
+    let ret = [''];
+    let lastNum = -1;
+    word.split('').forEach(function (ch, idx) {
+        let cur = parseInt(ch);
+        if (!isNaN(cur)) {
+            if (lastNum + 1 == idx) {
+                ret[ret.length - 1] += cur;
+            }
+            else {
+                ret.push(ch);
+            }
+            lastNum = idx;
+        }
+    });
+    return new Set(ret.filter(Boolean).map(function (el) { return parseInt(el); })).size;
+};
