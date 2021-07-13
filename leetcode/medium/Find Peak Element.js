3872534007884ddc7b738e23b58e8d801aea542f
Frellys <50921387+Frellys@users.noreply.github.com>
2021-07-13 15:18:28 +0500

    Add files via upload

--- leetcode/medium/Find Peak Element.js
+++ leetcode/medium/Find Peak Element.js
@@ -8,8 +8,7 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
-    if (nums.length == 1) return 0;
-    if (nums[0] > nums[1]) return 0;
+    if (nums.length == 1 || nums[0] > nums[1]) return 0;
     if (nums[nums.length - 1] > nums[nums.length - 2]) return nums.length - 1;
     for (let i = 1; i < nums.length - 1; i++) {
         if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) return i;
