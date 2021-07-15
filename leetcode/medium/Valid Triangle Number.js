7b64db70c16fc74c2616ef033982f5e661ca82f6
Frellys <50921387+Frellys@users.noreply.github.com>
2021-07-15 16:17:26 +0500

    Add files via upload

--- leetcode/medium/Valid Triangle Number.js
+++ leetcode/medium/Valid Triangle Number.js
@@ -1,4 +1,5 @@
-// Given an array consists of non - negative integers, your task is to count the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+// Given an array consists of non - negative integers,
+// your task is to count the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
 
 /**
  * @param {number[]} nums
@@ -6,11 +7,13 @@
  */
 var triangleNumber = function (nums) {
     let ret = 0;
-    let len = nums.length;
-    for (let i = 0; i < len - 2; i++) {
-        if (nums[i]) for (let j = i + 1; j < len - 1; j++) {
-            if (nums[j]) for (let k = j + 1, fSum = nums[i] + nums[j]; k < len; k++) {
-                if (nums[k] && nums[k] < fSum) {
+    let len = (nums = nums.filter(n => n)).length;
+    let iCap = len - 2;
+    let jCap = len - 1;
+    for (let i = 0; i < iCap; i++) {
+        for (let j = i + 1; j < jCap; j++) {
+            for (let k = j + 1, fSum = nums[i] + nums[j]; k < len; k++) {
+                if (nums[k] < fSum) {
                     ret += (nums[j] + nums[k] > nums[i] && nums[k] + nums[i] > nums[j]);
                 }
             }
