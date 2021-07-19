a5a3247c7dd06d47545d7ced61c9e861b47eb062
Frellys <50921387+Frellys@users.noreply.github.com>
2021-07-19 16:59:27 +0500

    Add files via upload

---
+++ leetcode/easy/Maximum Number of Words You Can Type.js
@@ -0,0 +1,15 @@
+// There is a malfunctioning keyboard where some letter keys do not work.All other keys on the keyboard work properly.
+// Given a string text of words separated by a single space(no leading or trailing spaces)
+// and a string brokenLetters of all distinct letter keys that are broken,
+// return the number of words in text you can fully type using this keyboard.
+
+/**
+ * @param {string} text
+ * @param {string} brokenLetters
+ * @return {number}
+ */
+var canBeTypedWords = function (text, brokenLetters) {
+    return text.split(' ').filter(function (word) {
+        return word.split('').every(ch => !brokenLetters.includes(ch));
+    }).length;
+};
