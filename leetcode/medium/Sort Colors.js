dcbd63c1299fc71a7b5c3013f73b984771b9c1a4
Frellys <50921387+Frellys@users.noreply.github.com>
2021-10-27 17:29:07 +0500

    Add files via upload

--- leetcode/medium/Sort Colors.js
+++ leetcode/medium/Sort Colors.js
@@ -6,10 +6,5 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == 0) nums.unshift(nums.splice(i, 1)[0]);
-    }
-    for (let i = nums.length; i >= 0; i--) {
-        if (nums[i] == 2) nums.push(nums.splice(i, 1)[0]);
-    }
+    return nums.sort();
 };
