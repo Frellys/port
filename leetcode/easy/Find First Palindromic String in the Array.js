c92920a08cbe4828a13852033e6882c6845498f1
Frellys <50921387+Frellys@users.noreply.github.com>
2022-01-19 19:13:37 +0500

    Add files via upload

---
+++ leetcode/easy/Find First Palindromic String in the Array.js
@@ -0,0 +1,21 @@
+// Given an array of strings words, return the first palindromic string in the array.
+// If there is no such string, return an empty string "".
+// A string is palindromic if it reads the same forward and backward.
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function (words) {
+    let isPalindrome = function (word) {
+        let ret = true;
+        let cap = Math.floor(word.length / 2);
+        for (let i = 0; ret && i < cap; i++) {
+            if (word[i] != word[word.length - 1 - i]) {
+                ret = false;
+            }
+        }
+        return ret;
+    };
+    return words.find(w => isPalindrome(w)) || '';
+};
