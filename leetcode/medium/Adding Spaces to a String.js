72023604d3008caab85a17f83a1c3b7e0ffb0e51
Frellys <50921387+Frellys@users.noreply.github.com>
2022-01-31 15:03:03 +0500

    Add files via upload

---
+++ leetcode/medium/Adding Spaces to a String.js
@@ -0,0 +1,17 @@
+// You are given a 0 - indexed string s and a 0 - indexed integer array spaces that describes the indices in the original string where spaces will be added.
+// Each space should be inserted before the character at the given index.
+// - For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. Thus, we obtain "Enjoy Your Coffee".
+// Return the modified string after the spaces have been added.
+
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+var addSpaces = function (s, spaces) {
+    let arr = s.split('');
+    spaces.reverse().forEach(function (n) {
+        arr[n] = ' ' + arr[n];
+    });
+    return arr.join('');
+};
