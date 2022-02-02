0ecc58280162510ada8a01cb4d77a10e41acc876
Frellys <50921387+Frellys@users.noreply.github.com>
2022-02-02 18:20:44 +0500

    Add files via upload

---
+++ leetcode/medium/Maximum Twin Sum of a Linked List.js
@@ -0,0 +1,37 @@
+// In a linked list of size n, where n is even, the ith node(0 - indexed) of the linked list is known as the twin of the(n - 1 - i)th node, if 0 <= i <= (n / 2) - 1.
+// - For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
+// The twin sum is defined as the sum of a node and its twin.
+// Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function (head) {
+    let max = Number.MIN_SAFE_INTEGER;
+    let cur = head;
+    let cnt = 0;
+    let traverse = function (node) {
+        if (node) {
+            cnt++;
+            traverse(node.next);
+            if (cnt) {
+                let val = cur.val + node.val;
+                if (max < val) {
+                    max = val;
+                }
+                cur = cur.next;
+                cnt -= 2;
+            }
+        }
+    };
+    traverse(head);
+    return max;
+};
