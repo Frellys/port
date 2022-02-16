ae23e2536e344986508352bc640ce9881e8968fe
Frellys <50921387+Frellys@users.noreply.github.com>
2022-02-16 16:08:24 +0500

    Add files via upload

--- leetcode/easy/Single Number.js
+++ leetcode/easy/Single Number.js
@@ -6,19 +6,14 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
-    if (nums.length == 1) return nums[0];
-    nums.sort(function (a, b) {
-        return a - b;
-    });
-    let ret;
-    if (nums[0] != nums[1]) return nums[0];
-    if (nums[nums.length - 1] != nums[nums.length - 2]) return nums[nums.length - 1];
-    for (let i = 1; i < nums.length - 1; i++) {
-        if (nums[i] == nums[i + 1] || nums[i] == nums[i - 1]) continue;
-        else {
-            ret = nums[i];
-            break;
+    let seen = {};
+    nums.forEach(function (n) {
+        if ((n in seen)) {
+            delete seen[n];
         }
-    }
-    return ret;
+        else {
+            seen[n] = true;
+        }
+    });
+    return +Object.keys(seen).find(k => seen[k]);
 };
