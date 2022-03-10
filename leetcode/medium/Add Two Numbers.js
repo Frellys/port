d23e2b3c2f49e25f60aca429adeb87b8c27d74a1
Frellys <50921387+Frellys@users.noreply.github.com>
2022-03-10 09:58:33 +0500

    Add files via upload

--- leetcode/medium/Add Two Numbers.js
+++ leetcode/medium/Add Two Numbers.js
@@ -15,27 +15,14 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
-    let n1 = [];
-    let n2 = [];
-    function traverse(node, arr) {
-        if (node) {
-            traverse(node.next, arr);
-            arr.push(node.val);
-        }
-    };
-    traverse(l1, n1);
-    traverse(l2, n2);
-    let sum = [];
-    let sup = 0;
-    while (n1.length || n2.length) {
-        let tmp = ((n1.length) ? n1.pop() : 0) + ((n2.length) ? n2.pop() : 0) + sup;
-        sup = (tmp >= 10) ? 1 : 0;
-        tmp = tmp % 10;
-        sum.push(tmp);
+var addTwoNumbers = function (l1, l2, sup = 0) {
+    if (l1 || l2) {
+        let sum = (l1?.val || 0) + (l2?.val || 0) + sup;
+        return new ListNode((sum % 10), addTwoNumbers(l1?.next, l2?.next, +(sum > 9)));
     }
-    if (sup) sum.push(sup);
-    sum = sum.map(function (num) { return new ListNode(num); });
-    sum.forEach(function (ln, ldx) { ln.next = (ldx + 1 == sum.length) ? null : sum[ldx + 1]; });
-    return sum[0];
+    else {
+        if (sup) {
+            return new ListNode(sup);
+        }
+    }
 };
