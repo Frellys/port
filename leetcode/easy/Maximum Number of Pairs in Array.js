186e8b45d82b4b3926a579b35b4aae5a9568dedf
Frellys <50921387+Frellys@users.noreply.github.com>
2022-07-18 10:03:27 +0300

    Add files via upload

---
+++ leetcode/easy/Maximum Number of Pairs in Array.js
@@ -0,0 +1,23 @@
+// You are given a 0-indexed integer array nums.
+// In one operation, you may do the following:
+// - Choose two integers in nums that are equal.
+// - Remove both integers from nums, forming a pair.
+// The operation is done on nums as many times as possible.
+// Return a 0-indexed integer array answer of size 2 where answer[0] is the number of pairs that are formed
+// and answer[1] is the number of leftover integers in nums after doing the operation as many times as possible.
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberOfPairs = function (nums) {
+    let seen = Object.fromEntries(Array.from(Array.from(new Set(nums)).map(n => [n, 0])));
+    nums.forEach(n => seen[n]++);
+    return [
+        Object.entries(seen).reduce((acc, [key, val]) => {
+            seen[key] = (val % 2);
+            return acc + Math.floor(val / 2);
+        }, 0),
+        Object.values(seen).filter(v => v).length
+    ];
+};
