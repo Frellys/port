8ee4f6a4cdef618013a8f3cf835121ba763b4225
Frellys <50921387+Frellys@users.noreply.github.com>
2022-07-28 10:16:56 +0500

    Add files via upload

---
+++ leetcode/easy/Convert Sorted Array to Binary Search Tree.js
@@ -0,0 +1,25 @@
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+// A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+    if (nums.length) {
+        let mid = Math.floor(nums.length / 2);
+        return new TreeNode(
+            nums[mid],
+            sortedArrayToBST(nums.splice(0, mid)),
+            sortedArrayToBST(nums.splice(1))
+        );
+    }
+};
