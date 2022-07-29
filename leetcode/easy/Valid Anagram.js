dc38bef5d0d7fa9e32319cdb9638aaaef4c914e9
Frellys <50921387+Frellys@users.noreply.github.com>
2022-07-29 13:00:34 +0500

    Add files via upload

--- leetcode/easy/Valid Anagram.js
+++ leetcode/easy/Valid Anagram.js
@@ -8,8 +8,5 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    if (s.length != t.length) return false;
-    let s1 = s.split('').sort().join('');
-    let s2 = t.split('').sort().join('');
-    return s1 == s2;
+    return new Set(Array.from(arguments).map(el => el.split('').sort().join(''))).size == 1;
 };
