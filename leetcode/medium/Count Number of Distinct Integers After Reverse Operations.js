710e073c7e3ce89efb2e4d713ac40cc4ab1ccd7c
Frellys <50921387+Frellys@users.noreply.github.com>
2022-11-25 12:17:55 +0500

    Add files via upload

---
+++ leetcode/medium/Count Number of Distinct Integers After Reverse Operations.js
@@ -0,0 +1,13 @@
+// You are given an array nums consisting of positive integers.
+// You have to take each integer in the array, reverse its digits, and add it to the end of the array.
+// You should apply this operation to the original integers in nums.
+// Return the number of distinct integers in the final array.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countDistinctIntegers = function (nums) {
+    nums.forEach(n => nums.push(+n.toString().split('').reverse().join('')));
+    return new Set(nums).size;
+};
