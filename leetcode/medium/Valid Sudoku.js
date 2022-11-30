bd8e7e223a6b8d46fae81f97dcdea9b846531b69
Frellys <50921387+Frellys@users.noreply.github.com>
2022-11-30 11:44:17 +0500

    Add files via upload

--- leetcode/medium/Valid Sudoku.js
+++ leetcode/medium/Valid Sudoku.js
@@ -1,4 +1,5 @@
-// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+// Determine if a 9 x 9 Sudoku board is valid.
+// Only the filled cells need to be validated according to the following rules:
 // - Each row must contain the digits 1-9 without repetition.
 // - Each column must contain the digits 1-9 without repetition.
 // - Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
@@ -8,58 +9,21 @@
  * @return {boolean}
  */
 var isValidSudoku = function (board) {
-    let ret = true;
-    // check rows
-    board.forEach(function (row) {
-        if (!containsUniqueValues(row)) ret = false;
+    let containsUniqueValues = function (arr) {
+        let vals = arr.filter(el => el != '.');
+        return vals.length == new Set(vals).size;
+    };
+    let cols = new Array(board.length).fill(false).map((r, rdx) => {
+        return new Array(board.length).fill(false).map((c, cdx) => board[cdx][rdx]);
     });
-    if (!ret) return false;
-    // check cols
-    let cols = [];
-    for (let r = 0; r < board.length; r++) {
-        let curCol = [];
-        for (let c = 0; c < board[r].length; c++) {
-            curCol.push(board[c][r]);
-        }
-        cols.push(curCol);
-    }
-    cols.forEach(function (col) {
-        if (!containsUniqueValues(col)) ret = false;
-    });
-    if (!ret) return false;
-    // check boxes
-    let boxes = [];
-    for (let r = 0; r < 9; r += 3) {
-        for (let c = 0; c < 9; c += 3) {
-            boxes.push([
-                board[r + 0][c + 0], board[r + 0][c + 1], board[r + 0][c + 2],
-                board[r + 1][c + 0], board[r + 1][c + 1], board[r + 1][c + 2],
-                board[r + 2][c + 0], board[r + 2][c + 1], board[r + 2][c + 2]
-            ]);
-        }
-    }
-    boxes.forEach(function (box) {
-        if (!containsUniqueValues(box)) ret = false;
-    });
-    // return if all checks passed
-    return ret;
-    /**
-     * @param {string[]} arr
-     * @return {boolean}
-     */
-    function containsUniqueValues(arr) {
-        let els = [];
-        let res = true;
-        arr.forEach(function (elem) {
-            if (elem != '.') {
-                if (els.includes(elem)) {
-                    res = false;
-                }
-                else {
-                    els.push(elem);
-                }
-            }
+    let boxes = new Array(board.length).fill(false).map((b, bdx) => {
+        let r = parseInt(bdx / 3) * 3;
+        let c = bdx % 3 * 3;
+        return new Array(9).fill(false).map((el, edx) => {
+            let rx = r + parseInt(edx / 3);
+            let cx = c + (edx % 3);
+            return board[rx][cx];
         });
-        return res;
-    }
+    });
+    return [board, cols, boxes].every(el => el.every(containsUniqueValues));
 };
