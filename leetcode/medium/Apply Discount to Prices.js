adeb140b0a10d662a65ff01e49525d762bc94d31
Frellys <50921387+Frellys@users.noreply.github.com>
2023-01-02 10:06:53 +0500

    Add files via upload

---
+++ leetcode/medium/Apply Discount to Prices.js
@@ -0,0 +1,20 @@
+// A sentence is a string of single-space separated words where each word can contain digits, lowercase letters, and the dollar sign '$'. A word represents a price if it is a sequence of digits preceded by a dollar sign.
+// - For example, "$100", "$23", and "$6" represent prices while "100", "$", and "$1e5" do not.
+// You are given a string sentence representing a sentence and an integer discount.
+// For each word representing a price, apply a discount of discount% on the price and update the word in the sentence.
+// All updated prices should be represented with exactly two decimal places.
+// Return a string representing the modified sentence.
+// Note that all prices will contain at most 10 digits.
+
+/**
+ * @param {string} sentence
+ * @param {number} discount
+ * @return {string}
+ */
+var discountPrices = function (sentence, discount) {
+    let factor = (100 - discount);
+    return sentence.replace(/(:?^|\s)\$([0-9]*[.])?[0-9]+(?!\S)/g, (match) => {
+        let [prev, num] = match.split('$');
+        return `${prev}$${(num / 100 * factor).toFixed(2)}`;
+    });
+};
