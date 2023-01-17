8d03f57d340185df29c7543623453e973b725cd4
Frellys <50921387+Frellys@users.noreply.github.com>
2023-01-17 18:50:24 +0500

    Add files via upload

--- leetcode/easy/Reverse Words in a String III.js
+++ leetcode/easy/Reverse Words in a String III.js
@@ -5,9 +5,5 @@
  * @return {string}
  */
 var reverseWords = function (s) {
-    let words = s.split(' ');
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i].split('').reverse().join('');
-    }
-    return words.join(' ');
+    return s.split(' ').map(word => word.split('').reverse().join('')).join(' ');
 };
