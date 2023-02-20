b1a91e34f1851d61f1650699dbb87477f361dc85
Frellys <50921387+Frellys@users.noreply.github.com>
2023-02-20 14:20:59 +0500

    Add files via upload

--- leetcode/easy/Search Insert Position.js
+++ leetcode/easy/Search Insert Position.js
@@ -1,4 +1,6 @@
-// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// Given a sorted array of distinct integers and a target value,
+// return the index if the target is found.
+// If not, return the index where it would be if it were inserted in order.
 
 /**
  * @param {number[]} nums
@@ -6,12 +8,17 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-    if (target < nums[0]) return 0;
-    if (target > nums[nums.length - 1]) return nums.length;
+    if (target > nums[nums.length - 1]) {
+        return nums.length;
+    }
     for (let i = 0; i < nums.length; i++) {
-        if (target == nums[i]) return i;
-        if (nums[i + 1] && target > nums[i] && target < nums[i + 1]) {
-            return i + 1;
+        if (target <= nums[i]) {
+            return i;
+        }
+        else {
+            if (target < nums[i + 1]) {
+                return i + 1;
+            }
         }
     }
 };
